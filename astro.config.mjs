2f3b685ae89eab8c37708fa3539d5ad7cd64145c
Casey Watts <casey.s.watts@gmail.com>
2024-11-05 15:50:07 -0500

    astro setup step 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import lit from '@astrojs/lit';
+
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), lit(), mdx(), sitemap()]
+});
